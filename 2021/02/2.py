d975897212d19dd6144c6eebc7e8489651da55b3
Theo Henson <theodorehenson@protonmail.com>
2021-12-03 16:06:39 -0500

    Finish 03 p 2, formatting

--- 2021/02/2.py
+++ 2021/02/2.py
@@ -1,4 +1,4 @@
-l = ''
+l = []
 with open('input') as f:
     l = [x.strip() for x in f.readlines()]
 
@@ -6,12 +6,12 @@ x = 0
 y = 0
 a = 0
 for i in l:
-  s = i.split()
-  if s[0] == "forward":
-    x += int(s[1])
-    y += a * int(s[1])
-  if s[0] == "up":
-    a -= int(s[1])
-  if s[0] == "down":
-    a += int(s[1])
+    s = i.split()
+    if s[0] == "forward":
+        x += int(s[1])
+        y += a * int(s[1])
+    if s[0] == "up":
+        a -= int(s[1])
+    if s[0] == "down":
+        a += int(s[1])
 print(x * y)
--- 2021/03/2.py
+++ 2021/03/2.py
@@ -1,39 +1,37 @@
-l = ''
+l = []
 with open('input') as f:
     l = [x.strip() for x in f.readlines()]
 
-cb = [0] * 12
-for n in l:
-  for i in range(12):
-    if n[i] == '0':
-      cb[i] -= 1
-    else:
-      cb[i] += 1
+def get_ge(l):
+    cb = [0] * 12
+    for n in l:
+        for i in range(12):
+            if n[i] == '0':
+                cb[i] -= 1
+            else:
+                cb[i] += 1
+    g = ['0'] * 12
+    e = ['0'] * 12
+    for i in range(12):
+        if cb[i] >= 0:
+            g[i] = '1'
+        else:
+            e[i] = '1'
+    return (g, e)
 
-g = ['0'] * 12
-e = ['0'] * 12
-for i in range(12):
-  if cb[i] >= 0:
-    g[i] = '1'
-  else:
-    e[i] = '1'
+def get_o(t, gei):
+    for i in range(12):
+        ri = []
+        cc = get_ge(t)[gei]
+        for n in t:
+            if cc[i] != n[i]:
+                ri.append(n)
+        for r in ri:
+            if len(t) == 1:
+                return t[0]
+            t.remove(r)
+    return t[0]
 
-def get_o(t, cc):
-  for i in range(12):
-    ri = []
-    for n in t:
-      if cc[i] != n[i]:
-        ri.append(n)
-    for r in ri:
-      if len(t) == 1:
-        return t[0]
-      t.remove(r)
-  return t[1]
-
-o = get_o(l.copy(), g)
-c = get_o(l.copy(), e);
-
-print(''.join(g), ''.join(e))
-print(o, c)
-
+o = get_o(l.copy(), 0)
+c = get_o(l.copy(), 1)
 print(int(o, 2) * int(c, 2))
