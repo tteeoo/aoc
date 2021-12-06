cfcda963a6eca9e320d2ef1ed732ddddd73242e0
Theo Henson <34148527+tteeoo@users.noreply.github.com>
2021-12-06 16:58:44 +0000

    Day six

---
+++ 2021/06/1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+l = []
+with open('input') as f:
+    l = [int(x) for x in f.read().strip().split(',')]
+
+f = [0 for _ in range(9)]
+for i in l:
+  f[i] += 1
+
+for i in range(80):
+  old = f[0]
+  for j in range(len(f)):
+    if j == 8:
+      f[j] = old
+    elif j == 6:
+      f[j] = f[j+1] + old
+    else:
+      f[j] = f[j+1]
+print(sum(f))
